9f10835b0c6ed1d2368588ee6cde7816f81b104b
Brian Alvarado <bscanoa@gmail.com>
2021-11-21 15:18:47 -0500

    changes to export dataset

--- semana-3.R
+++ semana-3.R
@@ -1,8 +1,9 @@
-Nacidos_2019 <- read_csv("/mnt/s/stiven/2021-2/UNAL/PEF/Semana 3/Nacidos_2019.csv", 
-                         +     col_types = cols(TIPO_PARTO = col_character(), 
-                         +         IDFACTORRH = col_character()))
-# 3.1 Tabla de contingencia
+library(readr)
 
+Nacidos_2019 <- read_csv("/mnt/s/stiven/2021-2/UNAL/PEF/Semana 3/Nacidos_2019.csv", + col_types = cols(TIPO_PARTO = col_character(), + IDFACTORRH = col_character()))
+
+# 3.1 Tabla de contingencia
+View(Nacidos_2019)
 barplot(table(Nacidos_2019$IDFACTORRH)) # Diagrama de barras Factor RH
 barplot(table(Nacidos_2019$TIPO_PARTO)) # Diagrama de barras Tipo de Parto
 
@@ -30,6 +31,7 @@ pp_cesarea <- sum(tabla_contingencia[ , 2])
 pp_cesarea
 prob_cesarea <- pp_cesarea/total_datos
 prob_cesarea
+length(Nacidos_2019$TIPO_PARTO)
 
 # 3.4 Probabilidad de que un recién nacido nazca con factor RH Negativo
 
@@ -39,18 +41,19 @@ prob_rh_neg
 
 # 3.5 Probabilidad de si un bebé tiene RH negativo entonces este
 # nazca por parto espontáneo.
-(15101/rh_negativo)*100
+(15101/rh_negativo)
+
 
 # 3.6
 chisq.test(tabla_contingencia)
 
-
+chisq.test()?
 ji = chisq.test(tabla_contingencia)
 ji_c = ji$statistic
 
 total_datos <- sum(tabla_contingencia)
-filas = nrow(tabla_contingencia)
-colum = ncol(tabla_contingencia)
+filas <- nrow(tabla_contingencia)
+colum <- ncol(tabla_contingencia)
 
 # 3.7 índice V de Kramer:
 v_c = sqrt(ji_c/(total_datos*min((filas-1),(colum-1))))
@@ -60,4 +63,5 @@ v_c
 k_p = sqrt(ji_c/(ji_c + total_datos))
 k_p
 sqrt(((min(nrow(tabla_contingencia), ncol(tabla_contingencia))) -1)/min(nrow(tabla_contingencia), ncol(tabla_contingencia)))
+(0.7071068/5)*5
 
